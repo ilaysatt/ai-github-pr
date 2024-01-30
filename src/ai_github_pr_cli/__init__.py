ac6297aa890b2d83fcda6c68b9eec32f5dba1380
ilaysat <ilaysat@gmail.com>
2024-01-30 08:16:31 +0200

    Split openai chat into 2: one for comment and one for code suggestions

--- src/ai_github_pr_cli/__init__.py
+++ src/ai_github_pr_cli/__init__.py
@@ -25,9 +25,9 @@ def main():
     client = OpenAI(
         api_key=os.getenv('api_key')
     )
-    request = ("Generate clear, concise, and actionable PR comments that provide constructive feedback to the "
-               "developer. In addition to comments, suggest specific, practical, and easy-to implement changes "
-               "to improve the code. ")
+    request_comment = ("Generate clear, concise, and actionable PR comments that provide constructive feedback to the "
+                       "developer. ")
+    request_suggestion = "Suggest specific, practical, and easy-to implement changes to improve the code. "
 
     sys_messages = {"role": "system", "content": "You are a tool that analyzes changes in GitHub pull requests, "
                                                  "understanding the context and impact on the overall codebase. "}
@@ -35,32 +35,52 @@ def main():
         print(f"\nPull-request {content[0]}: {content[1]}")
         for file in content[2]:
             if not file[1]:
-                message = request + "This is the file: " + file[2] + (". Ignore the pluses and minuses in the "
-                                                                      "beginning of the"
-                                                                      "lines.")
+                message_comment = request_comment + "This is the file: " + file[2] + (". Ignore the pluses and "
+                                                                                      "minuses in the"
+                                                                                      "beginning of the"
+                                                                                      "lines.")
+                message_suggestion = request_suggestion + "This is the file: " + file[2] + (". Ignore the pluses and "
+                                                                                            "minuses in the"
+                                                                                            "beginning of the"
+                                                                                            "lines.")
             else:
-                message = request + "This is the patch, i.e. the changes made to the file: " + file[2] + (
+                message_comment = request_comment + "This is the patch, i.e. the changes made to the file: " + file[2] + (
+                    "\n This is the file "
+                    "content before the "
+                    "changes: ") + file[1]
+                message_suggestion = request_suggestion + "This is the patch, i.e. the changes made to the file: " + file[2] + (
                     "\n This is the file "
                     "content before the "
                     "changes: ") + file[1]
             encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
-            if len(encoding.encode(message)) > 4097:
+            if len(encoding.encode(message_comment)) > 4097 or len(encoding.encode(message_suggestion)) > 4097:
                 if file[1]:
-                    message = request + "This is the patch, i.e. the changes made to the file: " + file[2]
-                    if len(encoding.encode(message)) > 4097:
+                    message_comment = request_comment + "This is the patch, i.e. the changes made to the file: " + file[2]
+                    message_suggestion = request_suggestion + "This is the patch, i.e. the changes made to the file: " + file[2]
+                    if len(encoding.encode(message_comment)) > 4097 or len(encoding.encode(message_suggestion)) > 4097:
                         file.append(None)
                         continue
                 else:
                     file.append(None)
                     continue
             print(f"\nRegarding file: {file[0]}")
+            print("Processing comment...")
             chat_completion = client.chat.completions.create(
-                messages=[sys_messages, {"role": "user", "content": message}],
+                messages=[sys_messages, {"role": "user", "content": message_comment}],
                 model="gpt-3.5-turbo"
             )
             file.append(chat_completion.choices[0].message.content)
-            print(chat_completion.choices[0].message.content)
-        print("---------------")
+            print(file[-1])
+            print("Processing code suggestion...")
+            chat_completion = client.chat.completions.create(
+                messages=[sys_messages, {"role": "user", "content": message_suggestion}],
+                model="gpt-3.5-turbo"
+            )
+            file.append(chat_completion.choices[0].message.content)
+            print("Code suggestion:")
+            print(file[-1])
+            print("--------")
+        print("*****************")
     if args.upload:
         print("Uploading the comments to GitHub...")
         helper.upload_repo_pull_comments(pull_content, args.repo)
